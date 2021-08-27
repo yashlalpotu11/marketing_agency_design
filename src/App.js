ccc2471a64c8eefd15265996c526e590cfa7a212
YASH  LALPOTU <yashrlalpotu30@gmail.com>
2021-08-27 21:58:07 +0530

    Footer Ready

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import Home from './pages/HomePage/Home'
 import GlobalStyle  from './globalStyles'
-import {Navbar} from './components'
+import {Navbar, Footer} from './components'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
       <Router>
       <GlobalStyle/>
+      <ScrollToTop/>
       <Navbar/>
+      <Route exact  path="/react_marketing">
+        <Redirect to='/'/>
+      </Route>
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
+        <Footer/>
       </Router>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,2 +1,34 @@
-import React from 'react';
-import { Button } from '../../globalStyles';
+import React from 'react'
+import { Button } from "../../globalStyles"
+// import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from "react-icons/fa"
+import {
+    FooterContainer,
+    FooterSubscription,
+    FooterSubHeading,
+    FooterSubText,
+    FormInput,
+    Form,
+} from "./Footer.elements.js"
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterSubscription>
+                <FooterSubHeading>
+                    Join our exclusive membership to receive the latest news and trends
+                </FooterSubHeading>
+                <FooterSubText>
+                    You can unsubscribe at any time.
+                </FooterSubText>
+                <Form>
+                    <FormInput name="email" type="email" placeholder="Your Email" />
+                    <Button fontBig>Subscribe</Button>
+                </Form>
+            </FooterSubscription>
+        </FooterContainer>
+    )
+}
+
+export default Footer
+
+
